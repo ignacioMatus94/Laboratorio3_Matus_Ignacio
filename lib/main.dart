fbb211fb2bc471494023c50ffbb521e469fcf571
ignacio <ignismatus94@gmail.com>
2024-04-25 14:22:14 -0400

    lab5

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:hola/presentation/pantalla/contador.dart';
+import 'package:hola/presentation/pantalla/contador.dart'; 
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true,colorSchemeSeed: Colors.blue),
-      home: Contador(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contador'),
+      ),
+     body: Center(
+  child: Contador(),
+),
+      floatingActionButton: SizedBox(), 
     );
   }
 }
